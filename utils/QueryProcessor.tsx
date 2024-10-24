65ef047689f2438fdf6fd7fa3fd300c1430d2f20
Abdallah Abdaljalil <aabdalja@andrew.cmu.edu>
2024-10-24 16:20:04 +0300

    Added what is your name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,5 +12,11 @@ export default function QueryProcessor(query: string): string {
     //TODO update the corresponding test case in __tests__
     return ( "aabdalja" );
   }
+
+   if (query.includes("What is your name?")) {
+    //TODO add your Andrew ID below
+    //TODO update the corresponding test case in __tests__
+    return ( "aabdalja" );
+  }
   return "";
 }
